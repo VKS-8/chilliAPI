3941da23e5d30dcd111437c48e3cf9b6a9036d7e
VK Johnson <64655526+VKS-8@users.noreply.github.com>
2023-08-16 14:18:33 -0500

    Build server-side routes

--- server.js
+++ server.js
@@ -31,20 +31,41 @@ app.post('/chillies', (req, res) => {
   const newChilli = { id: count, ...req.body };
   count =count +1;
   chillies.push(newChilli);
+  console.log(chillies);
   res.send(newChilli);
 });
 
 app.put('/chillies/:id', (req, res) => {
-  console.log(`update a chilli with id ${req.params.id}`);
+  console.log(chillies);
+  const chilliIndex = chillies.findIndex((c) => c.id == req.params.id);
+
+  // If no id is found, send chilliMatch will be undefined which is falsy
+
+  if (chilliIndex != -1) {
+    chillies[chilliIndex] = req.body;
+    console.log(req.body);
+    res.send(chillies[chilliIndex]);
+  }else {
+    res.sendStatus(400);
+  }
 });
 
 app.delete('/chillies/:id', (req, res) => {
   console.log(`delete a chilli with id ${req.params.id}`);
+
+  let deletedChilli =chillies.find((c) => c.id == req.params.id);
+
+  if (!deletedChilli) {
+    res.sendStatus(400);
+  } else {
+    chillies = chillies.filter((c) => c.id != req.params.id);
+    res.send(deletedChilli);
+  }
 });
 
-
-
-
+app.all('/', (req, res) => {
+  res.sendStatus('404');
+});
 
 app.listen(3001, () => {
   console.log("listening on port 3001");
