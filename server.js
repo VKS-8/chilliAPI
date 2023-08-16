202b1128b4f07b6710918f867894a6efe49a148a
VK Johnson <64655526+VKS-8@users.noreply.github.com>
2023-08-16 11:24:13 -0500

    Create routes on server-side

---
+++ server.js
@@ -0,0 +1,47 @@
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+let chillies = [];
+let count = 0;
+app.get('/chillies', (req,res)=> {
+  res.send(chillies);
+  console.log('get all chillies');
+});
+
+app.get('/chillies/:id', (req, res) => {
+  console.log(`get a chilli with id ${req.params.id}`);
+  // check if a chilli with matching id was found
+  const chilliMatch = chillies.find((c) => c.id == req.params.id);
+
+  // if no id was found send chilliMatch will be undefined which is falsy
+
+  if(chilliMatch) {
+    res.send(chilliMatch);
+  } else {
+    res.sendStatus(400);
+  }
+});
+
+app.post('/chillies', (req, res) => {
+  console.log('add a chilli');
+});
+
+app.put('/chillies/:id', (req, res) => {
+  console.log(`update a chilli with id ${req.params.id}`);
+});
+
+app.delete('/chillies/:id', (req, res) => {
+  console.log(`delete a chilli with id ${req.params.id}`);
+});
+
+
+
+
+
+app.listen(3001, () => {
+  console.log("listening on port 3001");
+});
